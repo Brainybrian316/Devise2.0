38833622fed838460f2204904dafbe5e6fa506ea
Brainybrian316 <brainybrian316@gmail.com>
2022-07-27 18:12:19 -0400

    navbar

---
+++ client/src/components/Navbar/MenuItem.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+const Nav = ["Home", "IdeaBin", "DeviseProjects", "UserProjects", "Membership", "Contact", "Login", "Signup"];
+
+export const MenuItem = ({ i }) => {
+  return (
+    <motion.li
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <div className="links"  />
+      <span>{Nav[i]}</span>
+    </motion.li>
+ 
+  );
+};
